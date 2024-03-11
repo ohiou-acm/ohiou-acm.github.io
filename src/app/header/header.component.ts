c848af3bcdaa51e19c312778124ac3270d27ba26
Jaret Crist <jaret.crist@gmail.com>
2024-03-11 11:25:57 -0400

    Header progress

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
+}
